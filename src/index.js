7c6e7f7640d3aa5a3ad782007c3583231cfea552
princekumarpaswan <prasantandal123@gmail.com>
2023-02-20 01:36:04 +0530

    mobile complete

--- src/index.js
+++ src/index.js
@@ -5,4 +5,10 @@ const Click = () => {
     mobileMenu.classList.remove("hidden")
 }
 
+const ClickSide = () => {
+    const mobileMenu = document.getElementById("mobileMenu")
+    mobileMenu.classList.add("hidden")
+}
+
 const button = document.getElementById("button").addEventListener("click", Click)
+const crossbtn = document.getElementById("crossbtn").addEventListener("click", ClickSide)
